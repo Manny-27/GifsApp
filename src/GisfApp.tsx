7efab257defca2800141593e22b7cd10b9e083aa
Manny-27 <mannyalberto2763@gmail.com>
2025-09-25 12:07:35 -0300

    creating the handleSearch functionality

--- src/GisfApp.tsx
+++ src/GisfApp.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { GifList } from "./gifs/components/GifList";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
 import { mockGifs } from "./mock-type/gifs.mock"
@@ -6,16 +7,27 @@ import { SearchBar } from './shared/components/SearchBar';
 
 
 export const GisfApp = () => {
+
+  const [previousTerms, setpreviousTerms] = useState(['dragon ball z']);
+
+  const handleTermClicked = (term: string) => {
+    console.log({term});
+  }
+
+  const handleSearch = (query: string) => {
+    console.log(query)
+  }
+
   return (
     <>
             {/* Header */}
             <CustomHeader title="Buscador de Gifs" desciption=""/>
 
             {/* search */}
-            <SearchBar placeholder="Busca lo que quieras"/>
+            <SearchBar placeholder="Busca lo que quieras" onQuery={handleSearch}/>
 
             {/* busquedas previas */}
-            <PreviousSearches searches={["goku", "naruto", "rengoku"]}/>
+            <PreviousSearches searches={previousTerms} onLabelClicked={handleTermClicked}/>
 
             {/* Gifs */}
             <GifList gifs={mockGifs}/>
--- src/shared/components/SearchBar.tsx
+++ src/shared/components/SearchBar.tsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
+
 interface Props {
   placeholder: string;
+
+  onQuery: (query: string) => void
 }
-export const SearchBar = ({placeholder = 'buscar'}: Props) => {
+
+export const SearchBar = ({placeholder = 'buscar', onQuery}: Props) => {
+  
+  const [query, setQuery] = useState('')
+  
+  const handleSearch = () => {
+    onQuery(query);
+    setQuery('')
+  }
+
+  const handleKeydown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if(event.key === "Enter") {
+      handleSearch();
+    }
+  }
+
   return (
     <div className="search-container">
-            <input type="text" placeholder={placeholder} />
-            <button>Buscar</button>
+            <input 
+            type="text" 
+            placeholder={placeholder} 
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+
+            onKeyDown={handleKeydown}
+            />
+            <button onClick={handleSearch}>Buscar</button>
     </div>
   )
 }
