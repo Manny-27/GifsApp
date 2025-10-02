3a0dc5872428edd294bc92e5d850ddf1ac685fea
Manny-27 <mannyalberto2763@gmail.com>
2025-10-02 10:45:39 -0300

    creating customHeader test

---
+++ src/shared/components/CustomHeader.test.tsx
@@ -0,0 +1,18 @@
+import { describe, test } from "vitest";
+
+describe("customHeader", () => {
+    test('should render the title correctly', () => {
+      //
+    })
+    
+
+    test('should render whe descriptin is provided', () => {
+      //
+    })
+
+    test('should not render when desciption not provided', () => {
+      //
+    })
+    
+    
+})
